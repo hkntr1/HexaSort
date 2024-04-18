465b37c44d73f42acbccbeadfb9054a4049a3a2b
Hakan Kantar <hakankantar@outlook.com.tr>
2024-04-18 08:35:39 +0300

    WaveController revised

--- Assets/Scripts/WaveController.cs
+++ Assets/Scripts/WaveController.cs
@@ -4,11 +4,12 @@ using UnityEngine;
 public class WaveController : MonoBehaviour
 {   
     Camera mainCamera;
-    public GameObject target;
+    public Stack stack;
     private Vector3 hitPointLeft, hitPointRight;
     [SerializeField] private LayerMask targetLayer;
     [SerializeField] private int numberOfTargets = 3; 
     [SerializeField] private float sideMargin = 0.2f;
+    public List<Stack> stacks;
     public List<Vector3> spawnPoints;
 
     void Start()
@@ -25,18 +26,14 @@ public class WaveController : MonoBehaviour
         Ray rightRay = mainCamera.ScreenPointToRay(new Vector3(Screen.width, Screen.height / 5, 0));
 
         RaycastHit hitLeft, hitRight;
-
-       
+        
         if (Physics.Raycast(leftRay, out hitLeft, Mathf.Infinity, targetLayer))
         {
             hitPointLeft = hitLeft.point;
-            Debug.Log(hitPointLeft);
         }
-
         if (Physics.Raycast(rightRay, out hitRight, Mathf.Infinity, targetLayer))
         {
             hitPointRight = hitRight.point;
-            Debug.Log(hitPointRight);
         }
     }
 
@@ -55,7 +52,8 @@ public class WaveController : MonoBehaviour
             float xPos = hitPointLeft.x + sideMarginWidth + (usableWidth / (numberOfTargets - 1)) * i;
             Vector3 spawnPoint = new Vector3(xPos, hitPointRight.y+0.02f, hitPointRight.z);
             spawnPoints.Add(spawnPoint);
-            Instantiate(target, spawnPoint, Quaternion.identity);
+            Stack newStack = Instantiate(stack, spawnPoint, Quaternion.identity);
+            stacks.Add(newStack);
         }
     }
 }
